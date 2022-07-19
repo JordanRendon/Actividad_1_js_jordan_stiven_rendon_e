c718a0fddef2d830e7e883f93ac32ee0f51419ea
JordanRendon <jordanre7991@gmail.com>
2022-07-18 21:13:39 -0500

    esta es la solucion de la actividad 1 de javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+let name= 'Jordan'
+let lastName= 'Rendon'
+let dateOfBirth= '1997'
+let city= 'Medellin'
+
+console.log(`Hola, mi nombre es ${name} ${lastName}, vivo en ${city} y tengo  ${2022-dateOfBirth} años` )
